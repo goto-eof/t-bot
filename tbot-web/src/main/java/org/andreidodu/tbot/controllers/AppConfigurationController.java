41af4cd4dca181183340b1495616a60c9328ac0a
Andrei Dodu <andrei.dodu@hotmail.it>
2022-04-14 10:58:54 +0200

    Allowed cross origin connection + added Add url button

--- tbot-web/src/main/java/org/andreidodu/tbot/controllers/AppConfigurationController.java
+++ tbot-web/src/main/java/org/andreidodu/tbot/controllers/AppConfigurationController.java
@@ -3,10 +3,12 @@ package org.andreidodu.tbot.controllers;
 import org.andreidodu.tbot.service.AppConfigurationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(maxAge = 3600)
 @RestController
 @RequestMapping(path = "/app-configuration")
 public class AppConfigurationController {
